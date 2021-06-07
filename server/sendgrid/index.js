3dfb524f3309fe67931e14d0a47691172450318e
luvycunanan <luvycunanan@gmail.com>
2021-06-07 16:40:58 -0700

    restored sendgrid code and dependencies

--- server/sendgrid/index.js
+++ server/sendgrid/index.js
@@ -3,8 +3,6 @@ require('dotenv').config({path:__dirname+'/./../../.env'});
 const sendGridMail = require('@sendgrid/mail')
 sendGridMail.setApiKey(process.env.SG_KEY)
 
-console.log(process.env.SG_KEY)
-
 const msg = {
   to: 'partiapp.cloudinary@gmail.com',
   from: 'partiapp.cloudinary@gmail.com',
