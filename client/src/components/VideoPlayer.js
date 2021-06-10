5a0f0f4e1f8db881acb3221c4e8c2cc60dab7c16
Olb2 <olgaborysovska@gmail.com>
2021-06-10 16:53:48 -0700

    updated video uploader options to speed up the upload

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 const qs = require('qs');
 function VideoPlayer(props) {
-    // const url = `https://player.cloudinary.com/embed/?public_id=${props.options.publicId}&cloud_name=${props.options.cloudName}&player%5Bfluid%5D=true&player%5Bcontrols%5D=true&source%5Bsource_types%5D%5B0%5D=mp4`;
+
+    // TODO: 
+    // TRIM 
+    // ADD Stickers 
+    // DELETE video 
     const config = { cloud_name: "demo", public_id: "elephants", cloudinary: { cname: "myCname" }, player: { loop: true }, source: { source_types: ['mp4/h265', 'mp4']} };
     const params = qs.stringify(config);
     const url = "https://player.cloudinary.com/embed/?"+ params;
@@ -21,7 +25,9 @@ function VideoPlayer(props) {
 
         <div>
 			<iframe
-			src="https://player.cloudinary.com/embed/?cloud_name=demo&public_id=elephants&fluid=true&controls=true&source_types%5B0%5D=mp4"
+            
+			src="https://res.cloudinary.com/parti-app/video/upload/v1623368826/parti-app/jxfpte8dpnvtyijocntl.mp4"
+            // src="https://player.cloudinary.com/embed/?cloud_name=parti-app&public_id=cxkajd5jfjp1coyi3n0z&fluid=true&controls=true&source_types%5B0%5D=mp4"
 			width="640"
 			height="400"
 			allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
