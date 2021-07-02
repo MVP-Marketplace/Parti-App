ada518bf2bf30b98560d2054935369a7f066bd92
tracilevine <televine@gmail.com>
2021-07-02 13:59:38 -0400

    remove test

--- client/src/components/StyledComponents/Buttons/MediumGreenButton.js
+++ client/src/components/StyledComponents/Buttons/MediumGreenButton.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const MediumGreenButton = styled.button `
 width: 298px;
 height: 42px;
-background: #93E9BE, 100%;
+background: #93E9BE;
 box-shadow: 0px 4px 9px rgba(53, 53, 53, 0.2);
 border-radius: 6px;
 &:hover {
