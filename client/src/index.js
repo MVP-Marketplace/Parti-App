28c1de54fb985dfe72cecde7043b2ecd5886fe0a
Joseph Brimeyer <54119655+josephbrimeyer@users.noreply.github.com>
2021-07-07 21:32:55 -0700

    redid router to address heroku errors.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import History from "./components/History/History";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <Router History={History}>
-    <App />
-  </Router>,
+  <App />,
+
   document.getElementById("root")
 );
