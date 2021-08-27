386d70dbb7eebb1936c0f89fc8aafecb817034d2
SpideyFanDan <dsmith.teach@gmail.com>
2021-08-26 18:47:59 -0600

    worked out login issues and getting userID to recognize login

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,40 +1,52 @@
-const express = require('express')
-const passport = require('passport')
-const router = express.Router()
-const userController = require('../controllers/users')
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const userController = require('../controllers/users');
 
+router.get('/login', userController.login);
 
-router.get('/login', userController.login) 
+//user registers
+router.get('/register', userController.newUser);
+router.post('/register', userController.createUser);
 
-//user registers 
-router.get('/register', userController.newUser) 
-router.post('/register', userController.createUser)
-
-router.get('/logout', userController.logout)
+router.get('/logout', userController.logout);
 router.get('/:id', userController.view);
 
-router.post('/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) { return next(err); }
-        if (!user) { return res.redirect('login'); }
-        req.logIn(user, function(err) {
-        if (err) { return next(err); }
-        console.log(req.user)
-        res.send(req.user)
-        // return res.redirect('/');
-      });
-    })(req, res, next);
-  });
-
-router.get('/auth/google',
-  passport.authenticate('google', { scope: ["profile", "email" ] }));
-
-// GET /auth/google/callback
-router.get('/auth/google/return', 
-  passport.authenticate('google', { failureRedirect: 'users/login' }),
-  function(req, res) {
-    res.redirect('/');
+router.post('/login', function (req, res, next) {
+	console.log('login inside');
+	passport.authenticate('local', function (err, user, info) {
+		console.log(err);
+    if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return res.redirect('login');
+		}
+		console.log('user', user);
+		console.log('body', req.body);
+		req.logIn(user, function (err) {
+			if (err) {
+				return next(err);
+			}
+			console.log(req.user);
+			res.send(req.user);
+			// return res.redirect('/');
+		});
+	})(req, res, next);
 });
 
+router.get(
+	'/auth/google',
+	passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
-module.exports = router
+// GET /auth/google/callback
+router.get(
+	'/auth/google/return',
+	passport.authenticate('google', { failureRedirect: 'users/login' }),
+	function (req, res) {
+		res.redirect('/');
+	}
+);
+
+module.exports = router;
