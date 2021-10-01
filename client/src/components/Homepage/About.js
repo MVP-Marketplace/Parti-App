cd97c593f0864cc168bf66d5dac9be46173d15e9
Jack Gamboa <jgamboa929@gmail.com>
2021-10-01 03:24:59 -0500

    Update UI, make responsive for About, Showcase, StepByStep

--- client/src/components/Homepage/About.js
+++ client/src/components/Homepage/About.js
@@ -1,4 +1,4 @@
-import { Container, Card, Row, Col, Image } from "react-bootstrap";
+import { Container, Card, Row, Col } from "react-bootstrap";
 import { SmallGreenButton } from "../StyledComponents/Buttons/SmallGreenButton";
 import Photo4 from "../../stockPhotos/c-landingpage-photo.png";
 import History from "../History/History";
@@ -10,39 +10,49 @@ function About() {
       style={{
         backgroundImage: `url(${PurpleGreenBackground})`,
         backgroundSize: "120% 100%",
-        paddingTop: "150px",
+        paddingTop: "120px",
+        paddingBottom: "20px",
+        minHeight: "45vh",
       }}
     >
       <Container>
-        <Row className="justify-content-md-center m-3">
-          <Col className="justify-content-md-center m-3">
+        <Row className="flex-column flex-sm-column flex-md-row">
+          <Col>
             <Card
-              className="text-center"
-              border="0"
               style={{
-                width: "25rem",
-                marginLeft: "auto",
-                marginRight: "auto",
+                border: 0,
+                backgroundColor: "transparent",
+
+                margin: "0 auto 1rem",
+                textAlign: "center",
               }}
             >
-              <Image src={Photo4} />
+              <Card.Body>
+                <Card.Img
+                  src={Photo4}
+                  style={{
+                    width: "100%",
+                    minHeight: "20rem",
+                    maxHeight: "22rem",
+                    objectFit: "cover",
+                  }}
+                />
+              </Card.Body>
             </Card>
           </Col>
-          <Col className="justify-content-md m-3">
-            <Card
-              border="0"
-              style={{
-                width: "25rem",
-                backgroundColor: "transparent",
-                marginLeft: "auto",
-                marginRight: "auto",
-              }}
-            >
-              <Card.Title style={{ fontSize: 48 }}>
-                What is Parti Greetings?
-              </Card.Title>
+          <Col>
+            <Card style={{ border: 0, backgroundColor: "transparent" }}>
               <Card.Body>
-                <Card.Subtitle>
+                <Card.Title style={{ fontSize: "46px", fontWeight: 700 }}>
+                  What is Parti Greetings?
+                </Card.Title>
+                <Card.Subtitle
+                  style={{
+                    fontSize: ".8rem",
+                    fontWeight: 400,
+                    lineHeight: "1.3rem",
+                  }}
+                >
                   Parti provides a new way to digitally celebrate events. Remove
                   the need for video editing and time lost to long video
                   montages, and take back the enjoyment of the celebration by
@@ -50,10 +60,11 @@ function About() {
                   colleagues. You can also revisit your events anytime you want
                   and easily find the messages you'd like to see.
                 </Card.Subtitle>
-                <form>
+
+                <form className="d-flex justify-content-center justify-content-md-start">
                   <SmallGreenButton
                     onClick={() => History.push("/register")}
-                    className="m-3"
+                    className="mt-3 mb-4 "
                   >
                     Get Started
                   </SmallGreenButton>
--- client/src/components/Homepage/StepByStep.js
+++ client/src/components/Homepage/StepByStep.js
@@ -1,4 +1,4 @@
-import { Container, CardGroup, Row, Card } from "react-bootstrap";
+import { Container, Row, Card, Col } from "react-bootstrap";
 import { SmallGreenButton } from "../StyledComponents/Buttons/SmallGreenButton";
 import History from "../History/History";
 
@@ -7,79 +7,124 @@ import Photo2 from "../../images/Group 412.svg";
 import Photo3 from "../../images/Group 413.svg";
 import Photo4 from "../../images/Group 323.svg";
 
-
 function StepByStep() {
   return (
     <Container>
-      <Row className="justify-content-lg-center m-5">
-        <Card className="m-5" border="0">
-          <Card.Title style={{ fontSize: 36 }}>
+      <Row className="justify-content-center mt-5">
+        <Card
+          className="m-5"
+          border="0"
+          style={{ backgroundColor: "transparent" }}
+        >
+          <Card.Title style={{ fontSize: "24px", fontWeight: 700 }}>
             Here's how it works with step by step process
           </Card.Title>
         </Card>
       </Row>
-      <Row className="justify-content-lg-center m-5">
-        <CardGroup>
-          <Card className="m-3" border="0">
+
+      <Row
+        style={{ margin: "0 auto" }}
+        className="row-cols-s-2 row-cols-lg-4 justify-content-between"
+      >
+        <Col className="d-flex justify-content-center">
+          <Card
+            style={{
+              minWidth: "15rem",
+              border: "none",
+              backgroundColor: "transparent",
+            }}
+            className="m-3 px-2"
+          >
             <Card.Img
               variant="top"
               src={Photo1}
-              style={{ width: "12rem", minHeight: "12rem" }}
+              style={{ width: "100%", minHeight: "12rem" }}
             />
             <Card.Body>
               <Card.Title>Create</Card.Title>
-              <Card.Subtitle>
+              <Card.Subtitle style={{ maxWidth: "12rem" }}>
                 Want to celebrate a special someone? Create a digitial,
                 customizable Parti card!
               </Card.Subtitle>
             </Card.Body>
           </Card>
-          <Card className="m-3" border="0">
+        </Col>
+
+        <Col className="d-flex justify-content-center">
+          <Card
+            style={{
+              minWidth: "15rem",
+              border: "none",
+              backgroundColor: "transparent",
+            }}
+            className="m-3 px-2"
+          >
             <Card.Img
               variant="top"
               src={Photo2}
-              style={{ width: "12rem", minHeight: "12rem" }}
+              style={{ width: "100%", minHeight: "12rem" }}
             />
             <Card.Body>
               <Card.Title>Collaborate</Card.Title>
-              <Card.Subtitle>
+              <Card.Subtitle style={{ maxWidth: "12rem" }}>
                 Invite unlimited collaborators. They can use any smart device or
                 camera to record up to a 3 min video or upload pictures.
               </Card.Subtitle>
             </Card.Body>
           </Card>
-          <Card className="m-3" border="0">
+        </Col>
+
+        <Col className="d-flex justify-content-center">
+          <Card
+            style={{
+              minWidth: "15rem",
+              border: "none",
+              backgroundColor: "transparent",
+            }}
+            className="m-3 px-2"
+          >
             <Card.Img
               variant="top"
               src={Photo3}
-              style={{ width: "12rem", minHeight: "12rem" }}
+              style={{ width: "100%", minHeight: "12rem" }}
             />
             <Card.Body>
               <Card.Title>Edit</Card.Title>
-              <Card.Subtitle>
+              <Card.Subtitle style={{ maxWidth: "12rem" }}>
                 Organize the order of videos, pictures, choose a background
                 theme and don't forget to upload your own!
               </Card.Subtitle>
             </Card.Body>
           </Card>
-          <Card className="m-3" border="0">
+        </Col>
+
+        <Col className="d-flex justify-content-center">
+          <Card
+            style={{
+              minWidth: "15rem",
+              border: "none",
+              backgroundColor: "transparent",
+            }}
+            className="m-3 px-2"
+          >
             <Card.Img
               variant="top"
               src={Photo4}
-              style={{ width: "12rem", minHeight: "12rem" }}
+              style={{ width: "100%", minHeight: "12rem" }}
             />
             <Card.Body>
               <Card.Title>Send</Card.Title>
-              <Card.Subtitle>
+              <Card.Subtitle style={{ maxWidth: "12rem" }}>
                 The recipient enjoys the string of videos from all the
                 contributors. They can filter through individual posts or view
                 them all!
               </Card.Subtitle>
             </Card.Body>
           </Card>
-        </CardGroup>
+        </Col>
       </Row>
-      <Row className="justify-content-lg-center m-5">
+
+      <Row className="justify-content-center m-5">
         <form>
           <SmallGreenButton
             onClick={() => History.push("/register")}
