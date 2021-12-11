bb5feb1af787cf29b6607d82c89e2c6401aa6973
Jack Gamboa <jgamboa929@gmail.com>
2021-12-10 22:02:16 -0600

    Get card content for each card id in the array

--- client/src/components/Dashboard/CardDetails.js
+++ client/src/components/Dashboard/CardDetails.js
@@ -1,68 +1,75 @@
 //probably not necessary because the dashboard will look the same for either the organizer or the recipient
 
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AppContext } from '../../contexts/AppContext';
-import { SmallGreenButton } from '../StyledComponents/Buttons/SmallGreenButton';
-import { Container, Card, Row, Col } from 'react-bootstrap';
-import './dashboard.css';
-import VideoThumbnail from './VideoThumbnail';
-import CardPage from '../../pages/CardPage';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../contexts/AppContext";
+import { SmallGreenButton } from "../StyledComponents/Buttons/SmallGreenButton";
+import { Container, Card, Row, Col } from "react-bootstrap";
+import "./dashboard.css";
+import VideoThumbnail from "./VideoThumbnail";
+import CardPage from "../../pages/CardPage";
 
 //This dashboard should populate when the user is a recipient and not an organizer
 function CardDetails(props) {
-	const { greetingCard } = useContext(AppContext);
-	let cardId = props.card;
-	let cardIdIdx = props.index;
-	let card = greetingCard[cardIdIdx];
-	// console.log('greeting card?', greetingCard.config);
-	console.log('greeting card ID index:', cardIdIdx);
-	console.log('greeting card ID:', cardId);
-	console.log('greeting card info:', greetingCard);
+  // const { greetingCard } = useContext(AppContext);
+  // let cardId = props.card;
+  let cardId = props.card._id;
+  let cardIdIdx = props.index;
+  // let card = greetingCard[cardIdIdx];
+  // console.log('greeting card?', greetingCard.config);
+  console.log("greeting card ID index:", cardIdIdx);
+  console.log("greeting card ID:", cardId);
+  // console.log('greeting card info:', greetingCard);
 
-	return (
-		<Container>
-			<Card className='m-3'>
-				<Row xs={1} md={3}>
-					<Col className='card-thumbnail'>
-						{/* <VideoThumbnail /> */}
-						<Card.Img
-							src={greetingCard.theme}
-							className='img-fluid'
-							alt='theme'
-						/>
-					</Col>
-					<Col className='card-col' xl={4}>
-						<Card.Body>
-							{/* This should render cards connected to the user/role and pull the title, date sent/due and status (open/closed) */}
-							<Card.Text>Title: {greetingCard.title} </Card.Text>
-							<Card.Text>For: {greetingCard.recipientFirstName}</Card.Text>
-							<Card.Text>
-								Event Date:
-								<p>{greetingCard.dueDate}</p>
-							</Card.Text>
-							<Card.Text>
-								Status:
-								<p>{greetingCard.status}</p>
-							</Card.Text>
-						</Card.Body>
-					</Col>
-					<div className='card-col'>
-						<Col>
-							<Link to={`/card/${cardId}`}>
-								{/* this should populate the specific card name and then clicking on the button below should send you to the indvidual card page */}
-								<SmallGreenButton key={cardIdIdx} value={cardId}>
-									View Card {CardPage}
-								</SmallGreenButton>
-							</Link>
-							<Link to={`/card/${cardId}`}>
-								<SmallGreenButton>Edit Card {CardPage}</SmallGreenButton>
-							</Link>
-						</Col>
-					</div>
-				</Row>
-			</Card>
-		</Container>
-	);
+  return (
+    <Container>
+      <Card className="m-3">
+        <Row xs={1} md={3}>
+          <Col className="card-thumbnail">
+            {/* <VideoThumbnail /> */}
+            <Card.Img
+              // src={greetingCard.theme}
+              src={props.card.theme}
+              className="img-fluid"
+              alt="theme"
+            />
+            {/* <p>{props.card._id}</p> */}
+          </Col>
+          <Col className="card-col" xl={4}>
+            <Card.Body>
+              {/* This should render cards connected to the user/role and pull the title, date sent/due and status (open/closed) */}
+              {/* <Card.Text>Title: {greetingCard.title} </Card.Text> */}
+              <Card.Text>Title: {props.card.title} </Card.Text>
+              {/* <Card.Text>For: {greetingCard.recipientFirstName}</Card.Text> */}
+              <Card.Text>For: {props.card.recipientFirstName}</Card.Text>
+              <Card.Text>
+                Event Date:
+                {/* <p>{greetingCard.dueDate}</p> */}
+                <p>{props.card.dueDate}</p>
+              </Card.Text>
+              <Card.Text>
+                Status:
+                {/* <p>{greetingCard.status}</p> */}
+                <p>{props.card.status}</p>
+              </Card.Text>
+            </Card.Body>
+          </Col>
+          <div className="card-col">
+            <Col>
+              <Link to={`/card/${cardId}`}>
+                {/* this should populate the specific card name and then clicking on the button below should send you to the indvidual card page */}
+                <SmallGreenButton key={cardIdIdx} value={cardId}>
+                  View Card {CardPage}
+                </SmallGreenButton>
+              </Link>
+              <Link to={`/card/${cardId}`}>
+                <SmallGreenButton>Edit Card {CardPage}</SmallGreenButton>
+              </Link>
+            </Col>
+          </div>
+        </Row>
+      </Card>
+    </Container>
+  );
 }
 export default CardDetails;
--- server/models/GreetingCard.js
+++ server/models/GreetingCard.js
@@ -1,57 +1,59 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const greetingCardSchema = new Schema({
-
-  userId: {
-    type: String,
-    required: true,
-    ref: "User",
-  },
-  recipientEmail: {
-    type: String,
-    required: true
-  }, 
-  recipientFirstName: {
-    type: String,
-    required: true
-  }, 
-  recipientLastName: {
-    type: String,
-    required: true
-  }, 
-  dueDate: {
-    type: String,
-  },
-  contentList: [
-		{
-			type: mongoose.Schema.Types.Object,
-			ref: "Content",
-		}
-  ],
-  contactList: [
-    {
-      type: Schema.Types.ObjectId,
+const greetingCardSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
       ref: "User",
     },
-  ],
-  status: {
-    type: String,
-    enum: ['Incomplete', 'Completed', 'Sent'],
-    default: "Incomplete",
+    recipientEmail: {
+      type: String,
+      required: true,
+    },
+    recipientFirstName: {
+      type: String,
+      required: true,
+    },
+    recipientLastName: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: String,
+    },
+    contentList: [
+      {
+        // type: mongoose.Schema.Types.Object,
+        type: Schema.Types.ObjectId,
+        ref: "Content",
+      },
+    ],
+    contactList: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["Incomplete", "Completed", "Sent"],
+      default: "Incomplete",
+    },
+    occasion: {
+      type: String,
+      enum: ["Anniversary", "Birthday", "Graduation", "Promotion", "Wedding"],
+    },
+    title: {
+      type: String,
+    },
+    theme: {
+      type: String,
+    },
   },
-  occasion: {
-    type: String,
-    enum: ['Anniversary', 'Birthday', 'Graduation', 'Promotion', 'Wedding']
-  },
-  title: {
-    type: String
-  }, 
-  theme: {
-    type: String,
-  },
-},
-{ timestamps: true })
+  { timestamps: true }
+);
 
-const GreetingCard = mongoose.model('GreetingCard', greetingCardSchema)
-module.exports = GreetingCard
+const GreetingCard = mongoose.model("GreetingCard", greetingCardSchema);
+module.exports = GreetingCard;
